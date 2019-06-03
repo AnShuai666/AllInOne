f4d43f9a975284f90d7cb288d04320187c0db1ee
AnShuai666 <1028792866@qq.com>
2019-06-03 21:47:44 +0800

    不要build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/as/Qt5/clion_qt_test001
-# Build directory: /home/as/Qt5/clion_qt_test001/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(bin/QT_TEST001 "QT_TEST001")
-add_test(bin/QT_TEST002 "QT_TEST002")
